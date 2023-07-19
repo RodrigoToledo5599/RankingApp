6d0c7f975df9a2f6e6ce11fc51f96a0a186143ad
RodrigoToledo5599 <rodrigotmt89@gmail.com>
2023-07-19 00:59:10 -0300

    Add project files.

---
+++ RankingApp/Controllers/ItemController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RankingApp.Models;
+
+namespace RankingApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ItemController : ControllerBase
+    {
+        private static readonly IEnumerable<ItemModel> Items
+    }
+}
